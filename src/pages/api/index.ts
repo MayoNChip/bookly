f8ea4ef6ca86809dbfd4724d47fb59ddb7a8805e
Ido Cohen <idoic44@gmail.com>
2022-08-08 10:49:55 +0300

    added cors

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -6,7 +6,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await NextCors(req, res, {
     // Options
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "https://bookly-eq90lly13-mayonchip.vercel.app/",
+    origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
